306285acd0a7dfcf678b4c78707f6da8ac97d89f
Nidhi Modi <modini.tandg@gmail.com>
2023-11-03 15:38:49 +1300

    database

--- App.js
+++ App.js
@@ -6,66 +6,52 @@
  * @flow strict-local
  */
 
- import React, { Component } from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   PanResponder,
-   View,
- } from 'react-native';
- 
- import MainStackNavigator from './navigation/MainStackNavigator'
- import Database from './screens/Database'
- 
- 
- const db = new Database();
- 
- export default class App extends Component {
- 
-   constructor(props) {
-     super(props);
- 
-     this.state = {
- 
-       inactive: true,
-     }
- 
- 
-   }
-   componentDidMount() {
- 
- 
-   }
- 
-   
- 
- 
- 
-   render() {
-     return (
- 
-       <MainStackNavigator />
- 
-     );
-   }
- }
- 
- const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     justifyContent: 'center',
-     alignItems: 'center'
-   },
-   welcome: {
-     fontSize: 20,
-     textAlign: 'center',
-     margin: 10
-   },
-   instructions: {
-     textAlign: 'center',
-     marginBottom: 5,
-   },
- });
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  PanResponder,
+  View,
+} from 'react-native';
+
+import MainStackNavigator from './navigation/MainStackNavigator';
+import Database from './screens/Database';
+
+const db = new Database();
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inactive: true,
+    };
+  }
+  componentDidMount() {
+    db.initDB();
+  }
+
+  render() {
+    return <MainStackNavigator />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+});
